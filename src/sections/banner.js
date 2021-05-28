84eaece045e47a411c8ea3e9494dc60e39b2829d
weigriffiths <weilingriffiths@gmail.com>
2021-05-28 11:39:39 +0200

    cleanup

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,44 +1,9 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Container, Flex, Box, Heading, Input, Text, Image, Button } from 'theme-ui';
-import React, { useState } from 'react';
-import ModalVideo from 'react-modal-video';
-import { Link } from 'components/link';
-import { FaPlayCircle } from 'react-icons/fa';
 import BannerThumb from 'assets/banner-thumb.png';
 
-import client1 from 'assets/sponsor/paypal.svg';
-import client2 from 'assets/sponsor/google.svg';
-import client3 from 'assets/sponsor/dropbox.svg';
-
-const data = [
-  {
-    id: 1,
-    path: '#',
-    image: client1,
-    title: 'paypal',
-  },
-  {
-    id: 2,
-    path: '#',
-    image: client2,
-    title: 'google',
-  },
-  {
-    id: 3,
-    path: '#',
-    image: client3,
-    title: 'dropbox',
-  },
-];
-
 export default function Banner() {
-  // const [videoOpen, setVideoOpen] = useState(false);
-  // const handleClick = (e) => {
-  //   e.preventDefault();
-  //   setVideoOpen(true);
-  // };
-
   return (
     <section sx={styles.banner} id="home">
       <Container sx={styles.banner.container}>
@@ -50,10 +15,6 @@ export default function Banner() {
             The ultimate shopping experience. We deliver groceries to you at your
             convenience.
           </Text>
-          {/* <Text as="p" variant="heroPromo">
-          Be the first to get the app and get 50 zł off your first order and 
-          also be in the running to win 500zł of free groceries!
-          </Text> */}
           <Box as="form" sx={styles.banner.form} data-aos="fade-right">
               <Box as="label" htmlFor="subscribe" variant="styles.srOnly">
                 Get Invite
@@ -69,36 +30,6 @@ export default function Banner() {
                 Join waitlist
               </Button>
             </Box>
-          {/* <Flex>
-            <Button variant="whiteButton" aria-label="Get Started">
-              Get Started
-            </Button>
-            <>
-              <ModalVideo
-                channel="youtube"
-                isOpen={videoOpen}
-                videoId="ZNA9rmDsYVE"
-                onClose={() => setVideoOpen(false)}
-              />
-              <Button
-                variant="textButton"
-                aria-label="Watch Video"
-                onClick={handleClick}
-              >
-                <FaPlayCircle /> Watch Video
-              </Button>
-            </>
-          </Flex> */}
-          {/* <Flex sx={styles.sponsorBox}>
-            <Text sx={styles.sponsorTitle}>Sponsored by:</Text>
-            <Box sx={styles.sponsorBox.sponsor}>
-              {data.map((item, index) => (
-                <Link path={item.path} key={`client-key${index}`}>
-                  <Image src={item.image} alt={item.title} />
-                </Link>
-              ))}
-            </Box>
-          </Flex> */}
         </Box>
 
         <Box sx={styles.banner.imageBox}>
@@ -112,7 +43,6 @@ export default function Banner() {
 const styles = {
   banner: {
     overflow: ['hidden', 'initial', null, 'hidden'],
-    // backgroundImage: `url(${BannerBG})`,
     backgroundRepeat: `no-repeat`,
     backgroundPosition: 'top left',
     backgroundSize: 'cover',
@@ -125,7 +55,6 @@ const styles = {
       flexWrap: ['wrap', null, null, 'nowrap']
     },
     highlight: {
-      // background: 'linear-gradient(to top, #FFB586 40%, transparent 40%)' //half highlight
       backgroundColor: 'tertiary'
     },
     form: {
@@ -144,10 +73,7 @@ const styles = {
         boxShadow: '0px 10px 50px rgba(48, 98, 145, 0.08)',
       },
       button: {
-        
-        // cursor: 'pointer',
         fontSize: ['12px', null, null, null, 2, '16px'],
-        // borderRadius: ['4px'],
         padding: ['0 15px'],
         ml: ['10px'],
         width: ['90px', null, null, null, '180px'],
