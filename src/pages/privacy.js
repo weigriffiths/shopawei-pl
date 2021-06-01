c5861b4d5d6d2c6a1c23506368acb757e867fd33
weigriffiths <weilingriffiths@gmail.com>
2021-06-01 09:57:55 +0200

    add header

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -4,11 +4,13 @@ import { StickyProvider } from 'contexts/app/app.provider';
 import theme from 'theme';
 import Layout from 'components/layout';
 import Privacy from 'components/policies/privacy/Privacy'
+import Header from 'components/header/header-other'
 
 export default function PrivacyPage() {
   return (
     <ThemeProvider theme={theme}>
       {/* Add sticky header with just logo */}
+      <Header />
       <Privacy />
     </ThemeProvider>
   );
