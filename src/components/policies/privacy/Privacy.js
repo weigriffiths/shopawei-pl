05ca1d0a5c0b0a4db5720bb66920015b6e8433ae
weigriffiths <weilingriffiths@gmail.com>
2021-06-01 09:57:49 +0200

    style privacy policy

--- src/components/policies/privacy/Privacy.js
+++ src/components/policies/privacy/Privacy.js
@@ -1,42 +1,90 @@
 /** @jsx jsx */
 import { jsx, Heading, Box, Container, Text } from 'theme-ui';
 import { Link } from 'components/link';
-import BannerBG from 'assets/bannerBg.png';
+import privacyData from './privacy.data'
 
 export default function Privacy() {
   return (
-    <section sx={styles.banner} >
-      <Container sx={styles.head}>
-        <Box>
-          <Heading as="h3" variant="heroPrimary">Shopawei Privacy Policy</Heading>
-        </Box>
-      </Container>
-      <Container sx={styles.doc}>
-        <Box>
-          
-        </Box>
-      </Container>
-    </section>
+    <>
+      <section sx={styles.policy}>
+        <Container sx={styles.doc}>
+          <Box sx={styles.doc.version}>
+            <Text as="p">Version 1.0 - 1 Jun 2021</Text>
+            <Heading sx={styles.doc.title}>Polityka Prywatności Shopawei</Heading>
+            <Text as="h3" sx={styles.doc.subtitle}>Polityka prywatności opisuje zasady przetwarzania przez nas informacji na Twój temat, w tym danych osobowych oraz ciasteczek, czyli tzw. cookies.</Text>
+          </Box>
+          <Box sx={styles.doc.box}>
+            {privacyData.map(({header, info}, i) => (
+              <Box>
+                <Heading sx={styles.doc.heading}>{header}</Heading>
+                <nav>
+                  {info.map(({point, extras}, i) => (
+                    extras ? 
+                    (<ul><li>{point}<ul>{extras.map((text, info) => (
+                      <li>{text}</li>
+                    ))}</ul></li></ul>) :
+                    (<ul><li>{point}</li></ul>)
+                  ))}
+                </nav>
+              </Box>
+            ))}
+            <Box sx={styles.doc.links}>
+                <ul>
+                  <li><a href="https://support.microsoft.com/pl-pl/help/10607/microsoft-edge-view-delete-browser-history">Edge</a></li>
+                  <li><a href="https://support.microsoft.com/pl-pl/help/278835/how-to-delete-cookie-files-in-internet-explorer">Internet Explorer</a></li>
+                  <li><a href="http://support.google.com/chrome/bin/answer.py?hl=pl&amp;answer=95647">Chrome</a></li>
+                  <li><a href="http://support.apple.com/kb/PH5042">Safari</a></li>
+                  <li><a href="http://support.mozilla.org/pl/kb/W%C5%82%C4%85czanie%20i%20wy%C5%82%C4%85czanie%20obs%C5%82ugi%20ciasteczek">Firefox</a></li>
+                  <li><a href="http://help.opera.com/Windows/12.10/pl/cookies.html">Opera</a></li>
+                </ul>
+                <p>Urządzenia mobilne:</p>
+                <ul>
+                  <li><a href="http://support.google.com/chrome/bin/answer.py?hl=pl&amp;answer=95647">Android</a></li>
+                  <li><a href="http://support.apple.com/kb/HT1677?viewlocale=pl_PL">Safari (iOS)</a></li>
+                  <li><a href="http://www.windowsphone.com/pl-pl/how-to/wp7/web/changing-privacy-and-other-browser-settings">Windows Phone</a></li>
+                </ul>
+            </Box>
+          </Box>
+        </Container>
+      </section>
+    </>
   );
 }
 
 const styles = {
-  banner: {
-    overflow: ['hidden', 'initial', null, 'hidden'],
-    backgroundImage: `url(${BannerBG})`,
-    backgroundRepeat: `no-repeat`,
-    backgroundPosition: 'top left',
-    backgroundSize: 'cover',
-    pt: '150px',
-    pb: ['150px', null, null, '130px', '150px', '150px', '150px'],
-    backgroundColor: 'primary',
-  },
-  head: {
-    textAlign: 'center',
+  policy: {
+    pt: '96px',
+    pb: '48px',
+    backgroundColor: 'grey',
+    height: '100%'
   },
   doc: {
+    backgroundColor: 'white',
     borderRadius: '1.5rem',
     padding: '20px',
-
+    version: {
+      fontSize: '0.7rem'
+    },
+    title: {
+      mt: '20px',
+      textAlign: 'center',
+      color: 'heading_secondary'
+    },
+    subtitle: {
+      textAlign: 'center',
+      color: 'heading_secondary'
+    },
+    box: {
+      pt: '40px',
+      fontSize: '.8rem'
+    },
+    heading: {
+      fontSize: '1rem',
+      color: 'heading_secondary',
+      textTransform: 'uppercase'
+    },
+    links: {
+      pl: '40px'
+    }
   }
 }
