559b496807c839bea619dd533bfc56bfa5d4c2a7
weigriffiths <weilingriffiths@gmail.com>
2021-06-03 11:33:10 +0200

    add google analytics measure id

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga';
 
 export const initGA = () => {
   console.log('GA init');
-  ReactGA.initialize('UA-xxxxxxxxx-1');
+  ReactGA.initialize(process.env.GA_TRACKING_ID);
 };
 
 export const logPageView = () => {
