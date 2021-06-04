576ce8d1727aaa0fec950e31fcec4f140583973b
weigriffiths <weilingriffiths@gmail.com>
2021-06-04 08:10:00 +0200

    add email integration

--- src/components/sticky-form.js
+++ src/components/sticky-form.js
@@ -1,11 +1,132 @@
 /** @jsx jsx */
+import { useRef, useState } from 'react';
 import { jsx, Container, Flex, Box, Input, Button, Text } from 'theme-ui';
 
 export default function StickyForm({ className }) {
+  // 1. Create a reference to the input so we can fetch/clear it's value.
+  const inputEl = useRef(null);
+  // 2. Hold a status in state to handle the response from our API.
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null },
+  });
+
+  const handleMailChimpResponse = (errorMsg, successMsg) => {
+    if (errorMsg) {
+      // 4. If there was an error, update the message in state.
+      setStatus({
+        info: { error: true, msg: errorMsg },
+      });
+
+      return;
+    }
+
+    // 5. Clear the input value and show a success message.
+    setStatus({
+      submitted: true,
+      submitting: false,
+      info: { error: false, msg: successMsg },
+    });
+    // inputEl.current.value = '';
+  };
+
+  // const handleSendGridResponse = (status, msg) => {
+  //   if (status === 200) {
+  //     // 5. Clear the input value and show a success message.
+  //     setStatus({
+  //       submitted: true,
+  //       submitting: false,
+  //       info: { error: false, msg: msg },
+  //     });
+  //     inputEl.current.value = '';
+  //   } else {
+  //     setStatus({
+  //       info: { error: true, msg: msg },
+  //     });
+  //   }
+  // };
+
+  const subscribe = async (e) => {
+    e.preventDefault();
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+    //for mailChimp integration
+    const { error } = await res.json();
+    handleMailChimpResponse(
+      error,
+      'Success! 🎉 You are now subscribed to the newsletter.'
+    );
+    // For sendGrid integration
+    // const text = await res.text();
+    // handleSendGridResponse(res.status, text);
+  };
+
   return (
     <>
       <div sx={styles.stickyBox}>
-        <Text as="p" sx={styles.infoText} >
+        {!status.submitted ? (
+          <>
+            <Text as="p" sx={styles.infoText} >
+              Wpiz swój adres email w polu poniżej, powiadomimy Cię, kiedy aplikacja będzie gotowa
+            </Text>
+            <div sx={styles.stickyForm}>
+              <Box sx={styles.form}>
+                <form onSubmit={subscribe}>
+                  <Flex sx={styles.subscribeForm}>
+                    <label htmlFor="email" sx={{ variant: 'styles.srOnly' }}>
+                      Powiadom mnie
+                    </label>
+                    <Input
+                      sx={styles.form.input}
+                      ref={inputEl}
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Twój adres email..."
+                    />
+                    <Button
+                      sx={styles.inviteBtn}
+                      type="submit"
+                      disabled={status.submitting}
+                      variant="primary"
+                      aria-label="Powiadom mnie"
+                    >
+                      Powiadom mnie
+                    </Button>
+                  </Flex>
+                </form>
+              </Box>
+            </div>
+            <Box sx={styles.statusError}>
+              {status.info.error && (
+                  <div className="error">Komunikat błędu: {status.info.msg}</div>
+                )}
+                {!status.info.error && status.info.msg && (
+                  <div className="success">{status.info.msg}</div>
+              )}
+            </Box>
+          </>
+        ) : (
+          <Box>
+            <Text as="p" sx={styles.successText} >
+              Dziękujemy za zainteresowanie 😎, wkrótce otrzymasz od nas email.
+            </Text>
+          </Box>
+        )}
+        
+        
+        {/* <Text as="p" sx={styles.infoText} >
           Wpisz swój adres email w polu poniżej, powiadomimy Cię, kiedy aplikacja będzie gotowa
         </Text>
         <div sx={styles.stickyForm}>
@@ -23,7 +144,7 @@ export default function StickyForm({ className }) {
             Powiadom mnie
           </Button>
           </Box>
-        </div>
+        </div> */}
       </div>
     </>
   );
@@ -49,7 +170,7 @@ const styles = {
   },
   infoText: {
     display: ['block', null, null, null, 'none'],
-    fontSize: '.65rem',
+    fontSize: ['.5rem', '.65rem', null, null, null],
     color: 'primary',
     my: 1
   },
@@ -70,6 +191,20 @@ const styles = {
   },
   inviteBtn: {
     ml: '10px'
+  },
+  successText: {
+    opacity: 1,
+    py: 4,
+    color: 'tertiary',
+    fontSize: [1, 2, 2, null],
+    lineHeight: [2, null, null, null, 2.2],
+    transition: 'opacity 1s'
+  },
+  statusError: {
+    '.error': {
+      mt: '-10px',
+      color: 'error'
+    }
   }
 }
 
